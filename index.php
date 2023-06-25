6ddc43e2bedefd21240e879fa95c465d36fc71b8
chrisnahartanto <lolkan081@gmail.com>
2023-06-25 14:32:55 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est, nam recusandae maiores animi quis reiciendis voluptatum porro quasi libero aut dolor optio, consequuntur nisi odio alias nostrum sint tempora natus.</p>
+    <ul><li>satu</li></ul>
 </body>
 </html>
